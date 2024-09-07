e7bb5d57adf623d98bd22e78bd537e14d78597bc
DesiZafirova <desszafirowa@gmail.com>
2024-09-07 19:33:54 +0300

    helper function for parsing csv data created

---
+++ src/helpers/parseCSV.js
@@ -0,0 +1,15 @@
+function parseCSV(csvData) {
+  const rows = csvData.split('\n');
+  const columnHeaders = rows[0].split(',');
+
+  return rows.slice(1).map((row) => {
+    const values = row.split(',');
+
+    return columnHeaders.reduce((accObj, column, i) => {
+      accObj[column] = values[i];
+      return accObj;
+    }, {});
+  });
+}
+
+export default parseCSV;
