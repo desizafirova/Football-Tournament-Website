c331ba48e0b901527b9b827a43902c6cf8edec63
DesiZafirova <desszafirowa@gmail.com>
2024-09-07 21:12:27 +0300

    moving data into public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Homepage from './pages/Homepage';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Homepage />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,21 @@
+import styled from 'styled-components';
+import TournamentBracketsView from '../components/TournamentBracketsView';
+import useCSV from '../hooks/useCSV';
+
+const StyledHomepage = styled.main`
+  display: flex;
+`;
+
 function Homepage() {
-  return <div>Homepage</div>;
+  const matches = useCSV('../data/matches.csv');
+  const teams = useCSV('../data/teams.csv');
+  console.log(matches, teams);
+
+  return (
+    <StyledHomepage>
+      <TournamentBracketsView matches={matches} teams={teams} />
+    </StyledHomepage>
+  );
 }
 
 export default Homepage;
