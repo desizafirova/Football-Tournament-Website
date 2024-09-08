a18a7ac1db23720643d3946b1f6d1abdf36bb3f0
DesiZafirova <desszafirowa@gmail.com>
2024-09-08 13:54:05 +0300

    GlobalStyles imported and applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Homepage from './pages/Homepage';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Homepage />
     </>
   );
