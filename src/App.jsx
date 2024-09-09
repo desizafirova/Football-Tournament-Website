f39556870a4ddb5b192bfa50825b3202caf50846
DesiZafirova <desszafirowa@gmail.com>
2024-09-09 14:27:49 +0300

    Building Routes and importing GlobalContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Homepage from './pages/Homepage';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './components/AppLayout';
+import Homepage from './pages/Homepage';
+import MatchDetails from './pages/MatchDetails';
+import TeamDetails from './pages/TeamDetails';
+import { GlobalContextProvider } from './GlobalContext';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Homepage />
+      <GlobalContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to="/homepage" />} />
+              <Route path="/homepage" element={<Homepage />} />
+              <Route path="/match-details" element={<MatchDetails />} />
+              <Route path="team-details" element={<TeamDetails />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContextProvider>
     </>
   );
 }
