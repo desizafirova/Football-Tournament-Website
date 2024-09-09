5f52735097692ce183c4777129e87644a1ff7683
DesiZafirova <desszafirowa@gmail.com>
2024-09-09 13:18:48 +0300

    AppLayout component built

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,5 +1,22 @@
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLayout = styled.div`
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  margin: 0 auto;
+`;
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <StyledLayout>
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledLayout>
+  );
 }
 
 export default AppLayout;
