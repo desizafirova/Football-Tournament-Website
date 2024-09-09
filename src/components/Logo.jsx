23a0df1915d45c2ae77a992543cce11390a46b46
DesiZafirova <desszafirowa@gmail.com>
2024-09-09 16:20:37 +0300

    imported useNavigate and added handleClick event handler to the Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,22 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledLogo = styled.img`
   height: 5.5rem;
   margin: 1rem;
+  cursor: pointer;
 `;
 
 function Logo() {
-  return <StyledLogo src="/logo.svg" alt="Logo" />;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function handleClick() {
+    if (location.pathname === '/homepage') return;
+    navigate('/');
+  }
+
+  return <StyledLogo src="/logo.svg" alt="Logo" onClick={handleClick} />;
 }
 
 export default Logo;
