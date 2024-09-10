9ca775c0b4b0e628acdb607056fb244c8cc97a6a
DesiZafirova <desszafirowa@gmail.com>
2024-09-10 19:58:21 +0300

    SelectTeam component imported

--- src/pages/TeamDetails.jsx
+++ src/pages/TeamDetails.jsx
@@ -1,11 +1,12 @@
-import TeamDetailsTable from '../components/TeamDetailsTable';
+// import TeamDetailsTable from '../components/TeamDetailsTable';
 import ButtonBack from '../components/ButtonBack';
+import SelectTeam from '../components/SelectTeam';
 
 function TeamDetails() {
   return (
     <>
       <ButtonBack />
-      <TeamDetailsTable />
+      <SelectTeam />
     </>
   );
 }
