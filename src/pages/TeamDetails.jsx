1cc3c9aed78b82f8fa14bbbaafc208dee3f8f51d
DesiZafirova <desszafirowa@gmail.com>
2024-09-10 18:55:27 +0300

    fixed bug

--- src/pages/TeamDetails.jsx
+++ src/pages/TeamDetails.jsx
@@ -1,12 +1,7 @@
-import { useContext } from 'react';
 import TeamDetailsTable from '../components/TeamDetailsTable';
-import { GlobalContext } from '../GlobalContext';
 import ButtonBack from '../components/ButtonBack';
 
 function TeamDetails() {
-  const isLoadingTeams = useContext(GlobalContext);
-  if (isLoadingTeams) return <p>Loading...</p>;
-
   return (
     <>
       <ButtonBack />
