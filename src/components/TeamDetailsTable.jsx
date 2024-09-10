48e9b4e532533cc40acf821167ed79f7adbc8071
DesiZafirova <desszafirowa@gmail.com>
2024-09-10 19:56:57 +0300

    TeamDetailsTable component built

--- src/components/TeamDetailsTable.jsx
+++ src/components/TeamDetailsTable.jsx
@@ -1,15 +1,25 @@
 import { useContext } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import { GlobalContext } from '../GlobalContext';
+import Spinner from './Spinner';
 
 const StyledTeamDetailsTable = styled.table`
-  padding: 1rem;
   border: 2px solid var(--color-blue-200);
-  border-radius: var(--border-radius-md);
+  background-color: var(--color-blue-50);
+  margin: 0 auto;
+  border-collapse: collapse;
 `;
 
-function TeamDetailsTable() {
-  const { players, teams } = useContext(GlobalContext);
+function TeamDetailsTable({ teamId }) {
+  const { players, isLoadingPlayers } = useContext(GlobalContext);
+
+  if (isLoadingPlayers) return <Spinner />;
+
+  const currTeamPlayers = players.filter(
+    (player) => Number(player.TeamID) === teamId
+  );
+
   return (
     <StyledTeamDetailsTable>
       <thead>
@@ -20,16 +30,14 @@ function TeamDetailsTable() {
         </tr>
       </thead>
       <tbody>
-        {players.map((player) => {
-          const team = teams.find((team) => team.ID === player.TeamID);
-
-          return team ? (
+        {currTeamPlayers.map((player) => {
+          return (
             <tr key={player.ID}>
               <td>{player.TeamNumber}</td>
               <td>{player.FullName}</td>
               <td>{player.Position}</td>
             </tr>
-          ) : null;
+          );
         })}
       </tbody>
     </StyledTeamDetailsTable>
@@ -37,3 +45,7 @@ function TeamDetailsTable() {
 }
 
 export default TeamDetailsTable;
+
+TeamDetailsTable.propTypes = {
+  teamId: PropTypes.number.isRequired,
+};
