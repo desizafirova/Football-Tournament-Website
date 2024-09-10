6d146f3f9238129c4ff699da94ef65a215f491fa
DesiZafirova <desszafirowa@gmail.com>
2024-09-11 00:08:53 +0300

    changing the way of filtering the stages of the tournament

--- src/components/TournamentBracketsView.jsx
+++ src/components/TournamentBracketsView.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import TournamentStage from './TournamentStage';
 import { useContext } from 'react';
 import { GlobalContext } from '../GlobalContext';
+import parseDate from '../helpers/parseDate';
 
 const StyledTournamentBracketsView = styled.div`
   margin-top: 5rem;
@@ -11,14 +12,22 @@ const StyledTournamentBracketsView = styled.div`
 
 function TournamentBracketsView() {
   const { matches } = useContext(GlobalContext);
-  const stages = {
-    roundOf16: matches.slice(36, 44),
-    quarterFinals: matches.slice(44, 48),
-    semiFinals: matches.slice(48, 50),
-    final: matches.slice(50, 51),
-  };
 
-  console.log(stages.roundOf16, stages.quarterFinals);
+  function filterMatchesByDates(fromDateInput, toDateInput) {
+    const startDate = parseDate(fromDateInput);
+    const endDate = parseDate(toDateInput);
+    return matches.filter((match) => {
+      const matchDate = parseDate(match.Date);
+      return matchDate >= startDate && matchDate <= endDate;
+    });
+  }
+
+  const stages = {
+    roundOf16: filterMatchesByDates('6/29/2024', '7/2/2024'),
+    quarterFinals: filterMatchesByDates('7/5/2024', '7/6/2024'),
+    semiFinals: filterMatchesByDates('7/9/2024', '7/10/2024'),
+    final: filterMatchesByDates('7/14/2024', '7/14/2024'),
+  };
 
   return (
     <StyledTournamentBracketsView>
