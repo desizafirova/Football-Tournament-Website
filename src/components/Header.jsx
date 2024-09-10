0b662df1a664a9790646cbc35d0903e4a6fbb36a
DesiZafirova <desszafirowa@gmail.com>
2024-09-10 19:48:45 +0300

    MainNav imported

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import styled from 'styled-components';
 import Logo from './Logo';
+import MainNav from './MainNav';
 
 const StyledHeader = styled.header`
   background-color: var(--color-blue-700);
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
 function Header() {
   return (
     <StyledHeader>
       <Logo />
+      <MainNav />
     </StyledHeader>
   );
 }
