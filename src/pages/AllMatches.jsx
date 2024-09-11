61a20d9fe1d5c7b7d6f31240c913a03718e89805
DesiZafirova <desszafirowa@gmail.com>
2024-09-11 11:26:37 +0300

    Matches Table imported

--- src/pages/AllMatches.jsx
+++ src/pages/AllMatches.jsx
@@ -1,9 +1,11 @@
 import ButtonBack from '../components/ButtonBack';
+import MatchesTable from '../components/MatchesTable';
 
 function AllMatches() {
   return (
     <div>
       <ButtonBack>&larr; Back</ButtonBack>
+      <MatchesTable />
     </div>
   );
 }
