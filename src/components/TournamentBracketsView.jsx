81cfe6c4afdae7b53fab1e2187a2c9e96042c571
DesiZafirova <desszafirowa@gmail.com>
2024-09-11 15:38:01 +0300

    Imported filterMatchesByDate in order to filter the matches by date and to separate the matches in different stages. Imported getWinner helper function in order to get the winners from quarterFinals. Imported findMatchInStage helper function. Sorting the matches in stage roundOf16 in order to be properly presented in the UI.

--- src/components/TournamentBracketsView.jsx
+++ src/components/TournamentBracketsView.jsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
-import TournamentStage from './TournamentStage';
 import { useContext } from 'react';
 import { GlobalContext } from '../GlobalContext';
+import TournamentStage from './TournamentStage';
 import filterMatchesByDates from '../helpers/filterMatchesByDate';
+import getWinner from '../helpers/getWinner';
+import findMatchInStage from '../helpers/findMatchInStage';
 
 const StyledTournamentBracketsView = styled.div`
   margin-top: 5rem;
@@ -12,7 +14,6 @@ const StyledTournamentBracketsView = styled.div`
 
 function TournamentBracketsView() {
   const { matches } = useContext(GlobalContext);
-
   const stages = {
     roundOf16: filterMatchesByDates('6/29/2024', '7/2/2024', matches),
     quarterFinals: filterMatchesByDates('7/5/2024', '7/6/2024', matches),
@@ -20,10 +21,23 @@ function TournamentBracketsView() {
     final: filterMatchesByDates('7/14/2024', '7/14/2024', matches),
   };
 
+  const quarterFinalWinners = stages.quarterFinals.flatMap((match) => [
+    getWinner(match),
+    match.ATeamID,
+    match.BTeamID,
+  ]);
+
+  const sortedRoundOf16 = quarterFinalWinners.reduce((acc, teamID) => {
+    const roundOf16Match = findMatchInStage(teamID, stages.roundOf16);
+    return roundOf16Match && !acc.includes(roundOf16Match)
+      ? [...acc, roundOf16Match]
+      : acc;
+  }, []);
+
   return (
     <StyledTournamentBracketsView>
       <TournamentStage
-        matchesInStage={stages.roundOf16}
+        matchesInStage={sortedRoundOf16}
         tourStage="ROUND OF 16"
       />
       <TournamentStage
