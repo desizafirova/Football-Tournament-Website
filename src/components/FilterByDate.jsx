90d7bb1cf60065739d757f755897db95873fcd3e
DesiZafirova <desszafirowa@gmail.com>
2024-09-11 19:39:55 +0300

    FilterByDate component built

---
+++ src/components/FilterByDate.jsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+
+const FilterFlexContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 3rem;
+  margin-right: 2rem;
+`;
+
+const FilterBox = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+`;
+
+function FilterByDate({ onFilter }) {
+  const [fromDate, setFromDate] = useState('2024-06-14');
+  const [toDate, setToDate] = useState('2024-07-14');
+
+  useEffect(() => {
+    if (fromDate && toDate) {
+      onFilter({ fromDate, toDate });
+    }
+  }, [fromDate, toDate, onFilter]);
+
+  return (
+    <FilterFlexContainer>
+      <p>Filter by Date |</p>
+      <FilterBox>
+        <label htmlFor="fromDate">From:</label>
+        <input
+          id="fromDate"
+          type="date"
+          value={fromDate}
+          onChange={(e) => setFromDate(e.target.value)}
+          min="2024-06-14"
+          max="2024-07-14"
+        />
+      </FilterBox>
+      <FilterBox>
+        <label htmlFor="toDate">To:</label>
+        <input
+          type="date"
+          value={toDate}
+          onChange={(e) => setToDate(e.target.value)}
+          min="2024-06-14"
+          max="2024-07-14"
+        />
+      </FilterBox>
+    </FilterFlexContainer>
+  );
+}
+
+export default FilterByDate;
+
+FilterByDate.propTypes = {
+  onFilter: PropTypes.func.isRequired,
+};
